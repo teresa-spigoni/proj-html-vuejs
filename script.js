b34c5249003c01b9921e57f5436a2d4dd5d34af2
Teresa Spigoni <teresaspigoni@gmail.com>
2021-02-23 12:10:46 +0100

    footer

--- script.js
+++ script.js
@@ -2,6 +2,29 @@ new Vue({
   el: '#app',
   data: {
     nav: ['LANDING', 'PAGES', 'DOCS'],
+    social: [
+      {"name": "fab fa-facebook-f"},
+      {"name": "fab fa-instagram"},
+      {"name": "fab fa-twitter"},
+      {"name": "fab fa-linkedin-in"}
+    ],
+    company: [
+      {"option": "About us"},
+      {"option": "Services"},
+      {"option": "Team"},
+      {"option": "Pricing"},
+      {"option": "Project"},
+      {"option": "Careers"},
+      {"option": "Blog"},
+      {"option": "Login"}
+    ],
+    usefullLinks: [
+      {"name": "Terms of Services"},
+      {"name": "Privacy Policy"},
+      {"name": "Documentation"},
+      {"name": "Changelog"},
+      {"name": "Components"}
+    ]
   }
 });
 Vue.config.devtools = true;
